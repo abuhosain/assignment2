52766ba9d426bd326e70bcca4f87d86207fcebe9
Abu Hosain <crypticmin925@gmail.com>
2024-05-22 21:55:06 +0600

    router path msg done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from "express"
 import cors from "cors"
-import { ProductRouter } from "./app/modules/product/product.route";
+import { ProductRouter, notFoundRouter } from "./app/modules/product/product.route";
 import { OrderRouter } from "./app/modules/order/order.route";
 const app : Application = express()
  
@@ -12,6 +12,7 @@ app.use(cors())
 app.use("/api", ProductRouter)
 app.use("/api", OrderRouter)
 
+app.use(notFoundRouter);
 
 const getAController = (req : Request, res : Response) => {
     const a = 10
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { ProductController } from "./product.conrtoller";
 
 const router = express.Router();
@@ -8,6 +8,14 @@ router.get("/products/", ProductController.getAllProduct);
 router.get("/products/:productId", ProductController.getSingleProduct);
 router.put("/products/:productId", ProductController.updateProduct);
 router.delete("/products/:productId", ProductController.deleteProduct);
+
+router.all('*', (req: Request, res: Response) => {
+    res.status(404).json({
+      success: false,
+      message: 'Route not found',
+    });
+  });
+  export const notFoundRouter = router;
 
 
 export const ProductRouter = router;
